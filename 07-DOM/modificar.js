1f7d575c4446f5af9cde2b4722b3bcd41fba3329
FerFerZ1985 <fernandosebastianzarate1985@gmail.com>
2025-12-24 11:13:59 -0300

    modificarDom

--- 07-DOM/modificar.js
+++ 07-DOM/modificar.js
@@ -8,5 +8,9 @@ const box = document.querySelector('.box');
 // box.className = 'nuevo-class';
 
 // setAttribute(): Permite agregar o establecer el valor de un atributo
-box.setAttribute('class', 'box active mi-clase');
-box.setAttribute('data-id', '123-456-789');
+// box.setAttribute('class', 'box active mi-clase');
+// box.setAttribute('data-id', '123-456-789');
+
+// .style.propiedad: permite cambiar los estilos CSS de un elemento
+
+box.computedStyleMap.background = '#000'
